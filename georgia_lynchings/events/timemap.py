2fdf584abc51b16575dc6dab3af72ff8f7e06298
Sari Connard <sconna2@emory.edu>
2012-02-09 09:32:10 -0500

    Add timemap filtering.

--- georgia_lynchings/events/timemap.py
+++ georgia_lynchings/events/timemap.py
@@ -4,8 +4,10 @@ Tool for creating JSON Object for usage in Timemap display.
 import httplib2
 import json
 import logging
+from pprint import pprint
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from georgia_lynchings import geo_coordinates
 from georgia_lynchings.events.models import MacroEvent
@@ -15,18 +17,25 @@ logger = logging.getLogger(__name__)
 
 # solr fields we want for creating timemap json
 TIMEMAP_VIEW_FIELDS = [ 'row_id', 'label', 'min_date', 'max_date',
-                        'victim_county_brundage']
+                        'victim_county_brundage', 'victim_allegedcrime_brundage']
 
 class Timemap:
     '''The main class for creating json data for timemap display.
     '''
 
-    def __init__(self):
+    def __init__(self, filters=None):
         
         # Get a list of all the potential Macro Events to map
         self.me_list = []
         macs = MacroEvent.all_instances()
         for mac in macs: self.me_list.append(mac.id) 
+        
+        # Add filter capabilities
+        self.filterTags = {}        
+        if filters is None:
+            self.filters = []
+        else:
+            self.filters = filters
 
     def get_json(self):
         '''Get json object for timemap display.
@@ -58,13 +67,34 @@ class Timemap:
     def timemap_format(self, solr_items):
         ''' Format the solr results into a json structure for timemap.
         
-            :param solr_items: solr query result set      
+            :param solr_items: solr query result set
+            :param filters: list of filter tags            
         '''
         
+        ''' Set up a dictionary of filters to contain a dictionary of 
+        tags and their frequency
+        '''
+        
+        # Initialize filters for their tags and frequency
+        for fitem in self.filters:
+            self.filterTags[fitem] = {}
+
         # Timemap JSON Result initialization
-        jsonResult = [{"id":"event", "title":"Events","theme":"red","type":"basic","options":{'items':[]}}]     
+        jsonResult = [{"id":"event", "title":"Events","theme":"red","type":"basic","options":{'items':[]}}] 
+            
         for solr_item in solr_items:
             
+            # For each filter item create a dict with key=tags value=freq
+            all_tag_list = []
+            for fitem in self.filters:
+                # Create a frequency list of unique filter items, default to "Not Available"
+                tag_list = solr_item.get(fitem, ['Not Available'])  
+                for tag in set(tag_list):
+                    self.filterTags[fitem][tag.encode('ascii')] = \
+                        self.filterTags[fitem].get(tag.encode('ascii'), 0) + 1 
+                    all_tag_list.append(tag.encode('ascii'))
+                #TODO: for mulitple filters, find a way to distinguish tags w/in a filter
+
             # skip over if county or min_date is not defined  
             try:
                 # use first county in list.
@@ -74,7 +104,7 @@ class Timemap:
                 # populated with data. If that changes, we should use this 
                 # date as a backup for when min_date is not defined.            
                 if 'min_date' in solr_item and county in geo_coordinates.countymap:
-                    jsonResult[0]['options']['items'].append(self.create_timemap_item(solr_item, county))
+                    jsonResult[0]['options']['items'].append(self.create_timemap_item(solr_item, county, all_tag_list))
                 # Display missing data items
                 elif county not in geo_coordinates.countymap: 
                     logger.info(" Did not add macro event %s because county [%s] not defined in geo_coordinates" % (solr_item['row_id'], county))                    
@@ -82,9 +112,18 @@ class Timemap:
                     logger.info(" Did not add macro event %s because min_date not defined." % (solr_item['row_id']))
             except:
                 logger.info(" Timemap timemap_format error on macro event [%s]." % solr_item['row_id'])
+        
+        if settings.DEBUG:        
+            # This will output the list of filters with their tags and frequency to the log 
+            logger.debug("FILTERS:")       
+            for fitem in self.filters:
+                logger.debug("FILTER TAGS for %s:" % fitem)
+                for w in sorted(self.filterTags[fitem], key=self.filterTags[fitem].get, reverse=True):
+                    logger.debug("%-25s %d" % (w, self.filterTags[fitem][w]))            
+                              
         return jsonResult
         
-    def create_timemap_item(self, solr_item, county):
+    def create_timemap_item(self, solr_item, county, all_tag_list):
         '''Create a pinpoint item for timemap json file
         
         :param solr_item: a dictionary item from the solr query result set   
@@ -115,11 +154,16 @@ class Timemap:
         # Add location to pinpoint popup 
         moreinfo.append("<div>Location: %s County</div>" % county)
         
+        # Add tags to pinpoint popup
+        #print "\n ALL TAG LIST = [%s]\n" % ", ".join(all_tag_list)
+        tag_list = "[" + ", ".join(all_tag_list) + "]"         
+        moreinfo.append("<div>Tags: %s</div>" % tag_list)      
+        
         # FIXME: use reverse
         # reverse('events:details', kwargs={'id': solr_item['row_id']})
         moreinfo.append("<div><a target='_blank' href='../%s/details'>more info</a></div>" % solr_item['row_id'])
         moreinfo_link = ''.join(moreinfo)
         
-        item["options"]={'infoHtml': moreinfo_link.encode('ascii')}
+        item["options"]={'infoHtml': moreinfo_link.encode('ascii'), 'tags': tag_list.encode('ascii')} 
 
         return item
