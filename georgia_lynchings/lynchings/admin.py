80930732b84ceaafadebc3fe74f36427cd3216f1
Scott Turnbull <streamweaver@gmail.com>
2012-05-15 20:43:44 -0400

    More improved Model Admins

--- georgia_lynchings/lynchings/admin.py
+++ georgia_lynchings/lynchings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from georgia_lynchings.lynchings.models import County, Race, Accusation
+from georgia_lynchings.lynchings.models import County, Race, Accusation, \
+    Alias, Person, Lynching
 
 class CountyAdmin(admin.ModelAdmin):
     pass
@@ -11,9 +12,37 @@ class RaceAdmin(admin.ModelAdmin):
 class AccusationAdmin(admin.ModelAdmin):
     pass
 
+class AliasInline(admin.TabularInline):
+    model = Alias
+
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        ('Personal Attributes', {
+            'fields': ('name', ('race', 'gender', 'age')),
+        }),
+        ('PC-ACE Attributes', {
+            'classes': ('collapse',),
+            'fields': ('pca_id',)
+        }),
+    )
+    inlines = [
+        AliasInline,
+    ]
+
+class LynchingAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Event Details', {
+            'fields': ('victim', ('county', 'date'), 'alleged_crime', 'alternate_counties', 'story', ),
+            }),
+        ('PC-ACE Attributes', {
+            'classes': ('collapse',),
+            'fields': ('pca_id',)
+        }),
+        )
+    filter_horizontal = ('alternate_counties',)
 
 admin.site.register(County, CountyAdmin)
 admin.site.register(Race, RaceAdmin)
 admin.site.register(Accusation, AccusationAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Lynching, LynchingAdmin)
