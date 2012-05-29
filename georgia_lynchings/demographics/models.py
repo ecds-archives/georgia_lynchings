9c1278f0dd62440b8e400acd7050c26289189f06
Streaweaver <streamweaver@gmail.com>
2012-05-28 23:39:49 -0400

    Added admin models and imports for demographics.

--- georgia_lynchings/demographics/models.py
+++ georgia_lynchings/demographics/models.py
@@ -16,8 +16,8 @@ class County(models.Model):
     Represents data about a particular county.
     """
     name = models.CharField(max_length=50, help_text="Name of County")
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True, help_text="Latitude for geographic center of county.")
+    longitude = models.FloatField(null=True, blank=True, help_text="Longitude for geographic center of county.")
 
     # String Methods
     def __unicode__(self):
@@ -33,17 +33,25 @@ class Population(models.Model):
     """
     Cencus data from particular counties in particular years.
     """
-    county = models.ForignKey(County, help_text="County that data is for.")
-    year = models.PositiveInteger(choices=YEAR_CHOICES,
+    county = models.ForeignKey(County, help_text="County that data is for.")
+    year = models.PositiveIntegerField(choices=YEAR_CHOICES,
         help_text="Year of Census data.")
-    total = models.PositiveInteger(null=True, blank=True,
+    total = models.PositiveIntegerField(null=True, blank=True,
         help_text="Total Population.")
-    white = models.PositiveInteger(null=True, blank=True,
-        null=True, blank=True,
+    white = models.PositiveIntegerField(null=True, blank=True,
         help_text="White Population.")
-    black = models.PositiveInteger(null=True, blank=True,
+    black = models.PositiveIntegerField(null=True, blank=True,
         help_text="Black Population.")
-    iltr_white = models.PositiveInteger(null=True, blank=True,
+    iltr_white = models.PositiveIntegerField(null=True, blank=True,
         help_text="Illiterate White Population.")
-    iltr_black = models.PositiveInteger(null=True, blank=True,
+    iltr_black = models.PositiveIntegerField(null=True, blank=True,
         help_text="Illiterate Black Population.")
+
+    # String Methods
+    def __unicode__(self):
+        return u'%s Cencus for %s County' % (self.year, self.county)
+    def __str__(self):
+        return smart_str(self.__unicode__())
+
+    class Meta:
+        ordering = ["county__name", "year"]
