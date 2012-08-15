53018bd5ceffa378e110ac1f8719a5eaee3e0465
Scott Turnbull <streamweaver@gmail.com>
2012-08-15 14:08:49 -0400

    Adjusted some settings and template values.

--- georgia_lynchings/reldata/urls.py
+++ georgia_lynchings/reldata/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('georgia_lynchings.reldata.views',
     url(r'^graph/$', 'graph', name='graph'),
     url(r'^graph/data/$', 'graph_data', name='graph_data'),
     url(r'^graph/events/$', 'event_lookup', name='event_lookup'),
-    url(r'^wordcloud/$', 'wordcloud', name='wordcloud'),
-    url(r'^wordcloud/data/$', 'cloud_data', name='cloud_data'),
+    # url(r'^wordcloud/$', 'wordcloud', name='wordcloud'),
+    # url(r'^wordcloud/data/$', 'cloud_data', name='cloud_data'),
 )
