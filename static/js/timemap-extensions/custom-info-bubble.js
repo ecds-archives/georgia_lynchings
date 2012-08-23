22dc4dfb4bf0a94505ca16677473e467d390b870
Kyle Bock <kylewbock@gmail.com>
2012-08-23 09:55:25 -0400

    removed clear fix from details window, not sure why it was there
    also converted cutom-info-bubble.js

--- static/js/timemap-extensions/custom-info-bubble.js
+++ static/js/timemap-extensions/custom-info-bubble.js
@@ -1,6 +1,6 @@
 /** Custom Info Bubble for time map to allow for sliding map instead of traditional info bubble
-*	Compatible with timemap.js
-*	
+*   Compatible with timemap.js
+*   
 */
 
 // static class vars and methods
@@ -9,78 +9,79 @@ window.TimeMapExt.RED_ICON = "http://www.google.com/intl/en_us/mapfiles/ms/icons
 window.TimeMapExt.GREEN_ICON = "http://www.google.com/intl/en_us/mapfiles/ms/icons/green-dot.png";
 window.TimeMapExt.current_marker = null;
 window.TimeMapExt.restoreCurrent = function() {
-	if (window.TimeMapExt.current_marker != null) {
-		window.TimeMapExt.current_marker.getNativePlacemark().setIcon(window.TimeMapExt.RED_ICON);
-	}
+    if (window.TimeMapExt.current_marker != null) {
+        window.TimeMapExt.current_marker.getNativePlacemark().setIcon(window.TimeMapExt.RED_ICON);
+    }
 }
 
 // wait for doc ready and only if TimeMap is defined
 $(function() {
-	if (window.TimeMap) {
-		TimeMapItem.openInfoWindowBasic = function() {
-		    // preserve this for use in callbacks
-			var self = this;
-			
-			// for use in icon swapping
-			if (window.TimeMapExt.current_marker != self) {
-				window.TimeMapExt.restoreCurrent();
-				window.TimeMapExt.current_marker = self;
-			}
-			
-			if (TimeMapExt.static_center == undefined) {
-				TimeMapExt.static_center = self.map.getCenter();
-				TimeMapExt.map = self.map;
-			}
-	
-			var map_container = $("#mapcontainer");
-			$("#details").css("height", map_container.height());
-			
-			// hack to get width in terms of width
-			var percent_width = 100*map_container.width()/map_container.parent().width();
-			
-			// change the marker, must go through native api (google v3) to do this
-			self.getNativePlacemark().setIcon(window.TimeMapExt.GREEN_ICON);
-			
-			// make sure to animate only if visible
-			if (percent_width == 100 && !$("#details").is(":visible")) {
-				map_container.animate({"width" : Math.round(map_container.width() * 0.75)}, 250, complete = function() {
-					$("#details").width(map_container.parent().width() - map_container.width());
-					$("#details").show();
-				});
-			}
-			
-			// populate details with rendered infoTemplate 
-			$("#detail-content").html(self.getInfoHtml());
-			
-			// listener for the close icon
-			$("#details img.close").click(function() {
-				// close info window and replace marker
-				self.closeInfoWindow();
-			});
-			
-			// set center on selected icon
-			// TODO: consider also calling a resize on the map itself to get new dimensions
-			self.map.setCenter(self.placemark.location);
-		};
+    if (window.TimeMap) {
+        TimeMapItem.openInfoWindowBasic = function() {
+            // preserve this for use in callbacks
+            var self = this;
+            
+            // for use in icon swapping
+            if (window.TimeMapExt.current_marker != self) {
+                window.TimeMapExt.restoreCurrent();
+                window.TimeMapExt.current_marker = self;
+            }
+            
+            if (TimeMapExt.static_center == undefined) {
+                TimeMapExt.static_center = self.map.getCenter();
+                TimeMapExt.map = self.map;
+            }
+    
+            var map_container = $("#mapcontainer");
 
-		TimeMapItem.closeInfoWindowBasic = function() {
-			// consider moving this to static function
-			var map_container = $("#mapcontainer");
-			var percent_width = 100*map_container.width()/map_container.parent().width();
-	
-			// :visible check fixes timemap scrolling
-		   	if (percent_width < 100 && $("#details").is(":visible")) {
-				$("#details").hide();
-				$("#mapcontainer").animate({"width" : "100%"}, 500, complete = function() {
-					if (TimeMapExt.static_center) {
-						TimeMapExt.map.setCenter(TimeMapExt.static_center);
-					}
-				});
-			}
-			
-			// always restore red icon
-			window.TimeMapExt.restoreCurrent();
-		}; //end TimeMapItem.closeInfoWindowBasic
-	}
+            $("#details").css("height", map_container.height());
+            
+            // hack to get width in terms of width
+            var percent_width = 100*map_container.width()/map_container.parent().width();
+            
+            // change the marker, must go through native api (google v3) to do this
+            self.getNativePlacemark().setIcon(window.TimeMapExt.GREEN_ICON);
+            
+            // make sure to animate only if visible
+            if (percent_width == 100 && !$("#details").is(":visible")) {
+                map_container.animate({"width" : Math.round(map_container.width() * 0.75)}, 250, complete = function() {
+                    $("#details").width(map_container.parent().width() - map_container.width());
+                    $("#details").show();
+                });
+            }
+            
+            // populate details with rendered infoTemplate 
+            $("#detail-content").html(self.getInfoHtml());
+            
+            // listener for the close icon
+            $("#details img.close").click(function() {
+                // close info window and replace marker
+                self.closeInfoWindow();
+            });
+            
+            // set center on selected icon
+            // TODO: consider also calling a resize on the map itself to get new dimensions
+            self.map.setCenter(self.placemark.location);
+        };
+
+        TimeMapItem.closeInfoWindowBasic = function() {
+            // consider moving this to static function
+            var map_container = $("#mapcontainer");
+            var percent_width = 100*map_container.width()/map_container.parent().width();
+    
+            // :visible check fixes timemap scrolling
+            if (percent_width < 100 && $("#details").is(":visible")) {
+                $("#details").hide();
+                $("#mapcontainer").animate({"width" : "100%"}, 500, complete = function() {
+                    if (TimeMapExt.static_center) {
+                        TimeMapExt.map.setCenter(TimeMapExt.static_center);
+                    }
+                });
+            }
+            
+            // always restore red icon
+            window.TimeMapExt.restoreCurrent();
+        }; //end TimeMapItem.closeInfoWindowBasic
+    }
 });
 
